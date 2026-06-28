d762f0828bbb61a62710f7f7a312121463356424
komed3 <webmaster@komed3.de>
2026-06-29 00:47:56 +0200

    declare named exports

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,11 @@ import { lookup } from './lookup';
 
 export type * from './types';
 
-export const usStates = { lookup, states };
+export const byCode = lookup.byCode.bind( lookup );
+export const byName = lookup.byName.bind( lookup );
+export const byFips = lookup.byFips.bind( lookup );
+export const filter = lookup.filter.bind( lookup );
+
+export const usStates = { states, lookup, byCode, byName, byFips, filter };
 
 export default usStates;
