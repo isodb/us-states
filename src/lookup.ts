955f4ed8f112515557891ef2abed5a5e9377ab25
komed3 <webmaster@komed3.de>
2026-06-29 00:06:19 +0200

    remove unused code

--- src/lookup.ts
+++ src/lookup.ts
@@ -15,10 +15,6 @@ export class Lookup {
     this.states = states;
   }
 
-  private createIndex < K extends keyof USState > ( key: K ) : ReadonlyMap< USState[ K ], USState > {
-    return new Map( this.states.map( state => [ state[ key ], state ] ) );
-  }
-
   private get < K extends 'code' | 'name' | 'fips' > ( key: K ) : ReadonlyMap< USState[ K ], USState > {
     return this.indexes[ key ] ??= new Map( this.states.map( state => [ state[ key ], state ] ) );
   }
