c948beecf3306f847893df68cc3a484659ccc471
komed3 <webmaster@komed3.de>
2026-06-28 20:08:21 +0200

    add type comments

--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,36 @@
+/**
+ * Administrative type.
+ */
 export type Type =
-  | 'state' | 'district' | 'territory';
+  | 'state'
+  | 'district'
+  | 'territory';
 
+/**
+ * U.S. Census region.
+ */
 export type Region =
-  | 'South' | 'West' | 'Midwest' | 'Northeast' | 'Territory';
+  | 'South'
+  | 'West'
+  | 'Midwest'
+  | 'Northeast'
+  | 'Territory';
 
+/**
+ * U.S. Census division or geographic subdivision.
+ */
 export type Division =
-  | 'Pacific' | 'Mountain' | 'East South Central' | 'West South Central' | 'New England'
-  | 'South Atlantic' | 'Middle Atlantic' | 'East North Central' | 'West North Central'
-  | 'Oceania' | 'Caribbean';
+  | 'Pacific'
+  | 'Mountain'
+  | 'East South Central'
+  | 'West South Central'
+  | 'New England'
+  | 'South Atlantic'
+  | 'Middle Atlantic'
+  | 'East North Central'
+  | 'West North Central'
+  | 'Oceania'
+  | 'Caribbean';
 
 export interface USState {
   code: string;
