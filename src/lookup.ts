2851e9276f5ba10cf5678503d041670851f77a72
komed3 <webmaster@komed3.de>
2026-06-28 20:26:08 +0200

    setup indexes

--- src/lookup.ts
+++ src/lookup.ts
@@ -2,8 +2,19 @@ import { states } from './data/us-states';
 import type { USState } from './types';
 
 export class Lookup {
+  private readonly states = states;
+  private readonly codeIndex: ReadonlyMap< USState[ 'code' ], USState >;
+  private readonly nameIndex: ReadonlyMap< USState[ 'name' ], USState >;
+  private readonly fipsIndex: ReadonlyMap< USState[ 'fips' ], USState >;
+
+  constructor () {
+    this.codeIndex = this.createIndex( 'code' );
+    this.nameIndex = this.createIndex( 'name' );
+    this.fipsIndex = this.createIndex( 'fips' );
+  }
+
   private createIndex < K extends keyof USState > ( key: K ) : ReadonlyMap< USState[ K ], USState > {
-    return new Map( states.map( state => [ state[ key ], state ] ) );
+    return new Map( this.states.map( state => [ state[ key ], state ] ) );
   }
 }
 
