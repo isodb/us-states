e52ff2287685ff425e256c8f720e56174aa09eef
komed3 <webmaster@komed3.de>
2026-06-28 19:47:59 +0200

    declare region + division types

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,15 @@
-interface USState {
+export type Type =
+  | 'state' | 'district' | 'territory';
+
+export type Region =
+  | 'South' | 'West' | 'Midwest' | 'Northeast' | 'Territory';
+
+export type Division =
+  | 'Pacific' | 'Mountain' | 'East South Central' | 'West South Central' | 'New England'
+  | 'South Atlantic' | 'Middle Atlantic' | 'East North Central' | 'West North Central'
+  | 'Oceania' | 'Caribbean';
+
+export interface USState {
   code: string;
   name: string;
   capital: string;
