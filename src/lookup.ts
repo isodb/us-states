f08f2b139dcfc9df7adc59166692c47019341826
komed3 <webmaster@komed3.de>
2026-06-28 23:52:57 +0200

    setup indexes

--- src/lookup.ts
+++ src/lookup.ts
@@ -1,3 +1,16 @@
-export class Lookup {}
+import { states } from './data/us-states';
+import type { USState } from './types';
+
+export class Lookup {
+  private readonly indexes = {
+    code: this.createIndex( 'code' ),
+    name: this.createIndex( 'name' ),
+    fips: this.createIndex( 'fips' )
+  } as const;
+
+  private createIndex < K extends keyof USState > ( key: K ) : ReadonlyMap< USState[ K ], USState > {
+    return new Map( states.map( state => [ state[ key ], state ] ) );
+  }
+}
 
 export const lookup = new Lookup();
