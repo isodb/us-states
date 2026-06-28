f98cef12b77ebc5c105deb3a55e5c83d03b5dc17
komed3 <webmaster@komed3.de>
2026-06-29 00:16:26 +0200

    declare types

--- src/lookup.ts
+++ src/lookup.ts
@@ -1,16 +1,20 @@
 import { states } from './data/us-states';
 import type { USState } from './types';
 
+type Code = USState[ 'code' ];
+type Name = USState[ 'name' ];
+type Fips = USState[ 'fips' ];
+
 type Indexes = {
-  code?: ReadonlyMap< USState[ 'code' ], USState >;
-  name?: ReadonlyMap< USState[ 'name' ], USState >;
-  fips?: ReadonlyMap< USState[ 'fips' ], USState >;
+  code?: ReadonlyMap< Code, USState >;
+  name?: ReadonlyMap< Name, USState >;
+  fips?: ReadonlyMap< Fips, USState >;
 };
 
 type Keys = {
-  codes?: ReadonlyArray< USState[ 'code' ] >;
-  names?: ReadonlyArray< USState[ 'name' ] >;
-  fips?: ReadonlyArray< USState[ 'fips' ] >;
+  codes?: ReadonlyArray< Code >;
+  names?: ReadonlyArray< Name >;
+  fips?: ReadonlyArray< Fips >;
 };
 
 export class Lookup {
@@ -26,15 +30,15 @@ export class Lookup {
     return this.indexes[ key ] ??= new Map( this.states.map( state => [ state[ key ], state ] ) );
   }
 
-  public get codes () : ReadonlyArray< USState[ 'code' ] > {
+  public get codes () : ReadonlyArray< Code > {
     return this.keys.codes ??= [ ...this.index( 'code' ).keys() ]; 
   }
 
-  public get names () : ReadonlyArray< USState[ 'name' ] > {
+  public get names () : ReadonlyArray< Name > {
     return this.keys.names ??= [ ...this.index( 'name' ).keys() ]; 
   }
 
-  public get fips () : ReadonlyArray< USState[ 'fips' ] > {
+  public get fips () : ReadonlyArray< Fips > {
     return this.keys.fips ??= [ ...this.index( 'fips' ).keys() ]; 
   }
 
@@ -46,15 +50,15 @@ export class Lookup {
     return this.index( by ).get( key );
   }
 
-  public byCode ( code: USState[ 'code' ] ) : USState | undefined {
+  public byCode ( code: Code ) : USState | undefined {
     return this.index( 'code' ).get( code );
   }
 
-  public byName ( name: USState[ 'name' ] ) : USState | undefined {
+  public byName ( name: Name ) : USState | undefined {
     return this.index( 'name' ).get( name );
   }
 
-  public byFips ( fips: USState[ 'fips' ] ) : USState | undefined {
+  public byFips ( fips: Fips ) : USState | undefined {
     return this.index( 'fips' ).get( fips );
   }
 }
