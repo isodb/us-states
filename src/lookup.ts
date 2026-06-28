bc348a5882142bd03e8f2364e691bd1a207bf4b7
komed3 <webmaster@komed3.de>
2026-06-28 20:23:26 +0200

    implement private index builder method

--- src/lookup.ts
+++ src/lookup.ts
@@ -1,3 +1,10 @@
-export class Lookup {}
+import { states } from './data/us-states';
+import type { USState } from './types';
+
+export class Lookup {
+  private createIndex < K extends keyof USState > ( key: K ) : ReadonlyMap< USState[ K ], USState > {
+    return new Map( states.map( state => [ state[ key ], state ] ) );
+  }
+}
 
 export const lookup = new Lookup();
