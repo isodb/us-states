7a4ae9866129ba10569426254acec2c7d215c3d5
komed3 <webmaster@komed3.de>
2026-06-28 19:58:05 +0200

    define all states and territories

--- src/data/us-states.ts
+++ src/data/us-states.ts
@@ -8,4 +8,444 @@ export const states = [ {
   division: 'East South Central',
   fips: '01',
   type: 'state'
+}, {
+  code: 'AK',
+  name: 'Alaska',
+  capital: 'Juneau',
+  region: 'West',
+  division: 'Pacific',
+  fips: '02',
+  type: 'state'
+}, {
+  code: 'AZ',
+  name: 'Arizona',
+  capital: 'Phoenix',
+  region: 'West',
+  division: 'Mountain',
+  fips: '04',
+  type: 'state'
+}, {
+  code: 'AR',
+  name: 'Arkansas',
+  capital: 'Little Rock',
+  region: 'South',
+  division: 'West South Central',
+  fips: '05',
+  type: 'state'
+}, {
+  code: 'CA',
+  name: 'California',
+  capital: 'Sacramento',
+  region: 'West',
+  division: 'Pacific',
+  fips: '06',
+  type: 'state'
+}, {
+  code: 'CO',
+  name: 'Colorado',
+  capital: 'Denver',
+  region: 'West',
+  division: 'Mountain',
+  fips: '08',
+  type: 'state'
+}, {
+  code: 'CT',
+  name: 'Connecticut',
+  capital: 'Hartford',
+  region: 'Northeast',
+  division: 'New England',
+  fips: '09',
+  type: 'state'
+}, {
+  code: 'DE',
+  name: 'Delaware',
+  capital: 'Dover',
+  region: 'South',
+  division: 'South Atlantic',
+  fips: '10',
+  type: 'state'
+}, {
+  code: 'FL',
+  name: 'Florida',
+  capital: 'Tallahassee',
+  region: 'South',
+  division: 'South Atlantic',
+  fips: '12',
+  type: 'state'
+}, {
+  code: 'GA',
+  name: 'Georgia',
+  capital: 'Atlanta',
+  region: 'South',
+  division: 'South Atlantic',
+  fips: '13',
+  type: 'state'
+}, {
+  code: 'HI',
+  name: 'Hawaii',
+  capital: 'Honolulu',
+  region: 'West',
+  division: 'Pacific',
+  fips: '15',
+  type: 'state'
+}, {
+  code: 'ID',
+  name: 'Idaho',
+  capital: 'Boise',
+  region: 'West',
+  division: 'Mountain',
+  fips: '16',
+  type: 'state'
+}, {
+  code: 'IL',
+  name: 'Illinois',
+  capital: 'Springfield',
+  region: 'Midwest',
+  division: 'East North Central',
+  fips: '17',
+  type: 'state'
+}, {
+  code: 'IN',
+  name: 'Indiana',
+  capital: 'Indianapolis',
+  region: 'Midwest',
+  division: 'East North Central',
+  fips: '18',
+  type: 'state'
+}, {
+  code: 'IA',
+  name: 'Iowa',
+  capital: 'Des Moines',
+  region: 'Midwest',
+  division: 'West North Central',
+  fips: '19',
+  type: 'state'
+}, {
+  code: 'KS',
+  name: 'Kansas',
+  capital: 'Topeka',
+  region: 'Midwest',
+  division: 'West North Central',
+  fips: '20',
+  type: 'state'
+}, {
+  code: 'KY',
+  name: 'Kentucky',
+  capital: 'Frankfort',
+  region: 'South',
+  division: 'East South Central',
+  fips: '21',
+  type: 'state'
+}, {
+  code: 'LA',
+  name: 'Louisiana',
+  capital: 'Baton Rouge',
+  region: 'South',
+  division: 'West South Central',
+  fips: '22',
+  type: 'state'
+}, {
+  code: 'ME',
+  name: 'Maine',
+  capital: 'Augusta',
+  region: 'Northeast',
+  division: 'New England',
+  fips: '23',
+  type: 'state'
+}, {
+  code: 'MD',
+  name: 'Maryland',
+  capital: 'Annapolis',
+  region: 'South',
+  division: 'South Atlantic',
+  fips: '24',
+  type: 'state'
+}, {
+  code: 'MA',
+  name: 'Massachusetts',
+  capital: 'Boston',
+  region: 'Northeast',
+  division: 'New England',
+  fips: '25',
+  type: 'state'
+}, {
+  code: 'MI',
+  name: 'Michigan',
+  capital: 'Lansing',
+  region: 'Midwest',
+  division: 'East North Central',
+  fips: '26',
+  type: 'state'
+}, {
+  code: 'MN',
+  name: 'Minnesota',
+  capital: 'Saint Paul',
+  region: 'Midwest',
+  division: 'West North Central',
+  fips: '27',
+  type: 'state'
+}, {
+  code: 'MS',
+  name: 'Mississippi',
+  capital: 'Jackson',
+  region: 'South',
+  division: 'East South Central',
+  fips: '28',
+  type: 'state'
+}, {
+  code: 'MO',
+  name: 'Missouri',
+  capital: 'Jefferson City',
+  region: 'Midwest',
+  division: 'West North Central',
+  fips: '29',
+  type: 'state'
+}, {
+  code: 'MT',
+  name: 'Montana',
+  capital: 'Helena',
+  region: 'West',
+  division: 'Mountain',
+  fips: '30',
+  type: 'state'
+}, {
+  code: 'NE',
+  name: 'Nebraska',
+  capital: 'Lincoln',
+  region: 'Midwest',
+  division: 'West North Central',
+  fips: '31',
+  type: 'state'
+}, {
+  code: 'NV',
+  name: 'Nevada',
+  capital: 'Carson City',
+  region: 'West',
+  division: 'Mountain',
+  fips: '32',
+  type: 'state'
+}, {
+  code: 'NH',
+  name: 'New Hampshire',
+  capital: 'Concord',
+  region: 'Northeast',
+  division: 'New England',
+  fips: '33',
+  type: 'state'
+}, {
+  code: 'NJ',
+  name: 'New Jersey',
+  capital: 'Trenton',
+  region: 'Northeast',
+  division: 'Middle Atlantic',
+  fips: '34',
+  type: 'state'
+}, {
+  code: 'NM',
+  name: 'New Mexico',
+  capital: 'Santa Fe',
+  region: 'West',
+  division: 'Mountain',
+  fips: '35',
+  type: 'state'
+}, {
+  code: 'NY',
+  name: 'New York',
+  capital: 'Albany',
+  region: 'Northeast',
+  division: 'Middle Atlantic',
+  fips: '36',
+  type: 'state'
+}, {
+  code: 'NC',
+  name: 'North Carolina',
+  capital: 'Raleigh',
+  region: 'South',
+  division: 'South Atlantic',
+  fips: '37',
+  type: 'state'
+}, {
+  code: 'ND',
+  name: 'North Dakota',
+  capital: 'Bismarck',
+  region: 'Midwest',
+  division: 'West North Central',
+  fips: '38',
+  type: 'state'
+}, {
+  code: 'OH',
+  name: 'Ohio',
+  capital: 'Columbus',
+  region: 'Midwest',
+  division: 'East North Central',
+  fips: '39',
+  type: 'state'
+}, {
+  code: 'OK',
+  name: 'Oklahoma',
+  capital: 'Oklahoma City',
+  region: 'South',
+  division: 'West South Central',
+  fips: '40',
+  type: 'state'
+}, {
+  code: 'OR',
+  name: 'Oregon',
+  capital: 'Salem',
+  region: 'West',
+  division: 'Pacific',
+  fips: '41',
+  type: 'state'
+}, {
+  code: 'PA',
+  name: 'Pennsylvania',
+  capital: 'Harrisburg',
+  region: 'Northeast',
+  division: 'Middle Atlantic',
+  fips: '42',
+  type: 'state'
+}, {
+  code: 'RI',
+  name: 'Rhode Island',
+  capital: 'Providence',
+  region: 'Northeast',
+  division: 'New England',
+  fips: '44',
+  type: 'state'
+}, {
+  code: 'SC',
+  name: 'South Carolina',
+  capital: 'Columbia',
+  region: 'South',
+  division: 'South Atlantic',
+  fips: '45',
+  type: 'state'
+}, {
+  code: 'SD',
+  name: 'South Dakota',
+  capital: 'Pierre',
+  region: 'Midwest',
+  division: 'West North Central',
+  fips: '46',
+  type: 'state'
+}, {
+  code: 'TN',
+  name: 'Tennessee',
+  capital: 'Nashville',
+  region: 'South',
+  division: 'East South Central',
+  fips: '47',
+  type: 'state'
+}, {
+  code: 'TX',
+  name: 'Texas',
+  capital: 'Austin',
+  region: 'South',
+  division: 'West South Central',
+  fips: '48',
+  type: 'state'
+}, {
+  code: 'UT',
+  name: 'Utah',
+  capital: 'Salt Lake City',
+  region: 'West',
+  division: 'Mountain',
+  fips: '49',
+  type: 'state'
+}, {
+  code: 'VT',
+  name: 'Vermont',
+  capital: 'Montpelier',
+  region: 'Northeast',
+  division: 'New England',
+  fips: '50',
+  type: 'state'
+}, {
+  code: 'VA',
+  name: 'Virginia',
+  capital: 'Richmond',
+  region: 'South',
+  division: 'South Atlantic',
+  fips: '51',
+  type: 'state'
+}, {
+  code: 'WA',
+  name: 'Washington',
+  capital: 'Olympia',
+  region: 'West',
+  division: 'Pacific',
+  fips: '53',
+  type: 'state'
+}, {
+  code: 'WV',
+  name: 'West Virginia',
+  capital: 'Charleston',
+  region: 'South',
+  division: 'South Atlantic',
+  fips: '54',
+  type: 'state'
+}, {
+  code: 'WI',
+  name: 'Wisconsin',
+  capital: 'Madison',
+  region: 'Midwest',
+  division: 'East North Central',
+  fips: '55',
+  type: 'state'
+}, {
+  code: 'WY',
+  name: 'Wyoming',
+  capital: 'Cheyenne',
+  region: 'West',
+  division: 'Mountain',
+  fips: '56',
+  type: 'state'
+}, {
+  code: 'DC',
+  name: 'District of Columbia',
+  capital: 'Washington',
+  region: 'South',
+  division: 'South Atlantic',
+  fips: '11',
+  type: 'district'
+}, {
+  code: 'AS',
+  name: 'American Samoa',
+  capital: 'Pago Pago',
+  region: 'Territory',
+  division: 'Oceania',
+  fips: '60',
+  type: 'territory'
+}, {
+  code: 'GU',
+  name: 'Guam',
+  capital: 'Hagåtña',
+  region: 'Territory',
+  division: 'Oceania',
+  fips: '66',
+  type: 'territory'
+}, {
+  code: 'MP',
+  name: 'Northern Mariana Islands',
+  capital: 'Saipan',
+  region: 'Territory',
+  division: 'Oceania',
+  fips: '69',
+  type: 'territory'
+}, {
+  code: 'PR',
+  name: 'Puerto Rico',
+  capital: 'San Juan',
+  region: 'Territory',
+  division: 'Caribbean',
+  fips: '72',
+  type: 'territory'
+}, {
+  code: 'VI',
+  name: 'U.S. Virgin Islands',
+  capital: 'Charlotte Amalie',
+  region: 'Territory',
+  division: 'Caribbean',
+  fips: '78',
+  type: 'territory'
 } ] as const satisfies USState[];
